c41386bbdfdf55895e94767d941a3be6c7998565
kcryns <rayneskarlcydrick@gmail.com>
2023-05-10 15:33:51 +0800

    need edit pc

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,24 @@ toggleMenu.addEventListener("click", () => {
   headNav.classList.toggle("open");
   blurr.classList.toggle("open");
 });
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
+// const header = document.querySelector(".header");
+// const branding = document.querySelector(".branding img");
+// window.addEventListener("scroll", () => {
+//   console.log(window.pageYOffset);
+//   if (window.pageYOffset >= 100) {
+//     header.classList.add("blue");
+//   branding.style="background:red;";
+//   } else {
+//     header.classList.remove("blue");
+//   }
+// });
+
